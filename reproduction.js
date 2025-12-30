c82c80295a66d7ee315eda61cb54a133e6c76a74
bssdn7 <75687807+bssdn7@users.noreply.github.com>
2025-12-30 21:29:00 +0530

    Update reproduction.js

--- reproduction.js
+++ reproduction.js
@@ -1,19 +1,12 @@
-const mutation = require("./mutation");
-
 function mate(a,b){
-  const baby = {
-    name: "x"+Math.floor(Math.random()*100000),
-    born: Date.now(),
+  return {
+    name:"x"+Math.floor(Math.random()*100000),
+    born:Date.now(),
     coreTraits:{
-      chaosSensitivity:(a.coreTraits.chaosSensitivity + b.coreTraits.chaosSensitivity)/2,
-      learningRate:(a.coreTraits.learningRate + b.coreTraits.learningRate)/2,
-      darkAffinity:(a.coreTraits.darkAffinity + b.coreTraits.darkAffinity)/2
-    },
-    parents:[a.name,b.name],
-    currentLife:0,
-    fossilLives:[]
+      chaosSensitivity:(a.coreTraits.chaosSensitivity+b.coreTraits.chaosSensitivity)/2,
+      learningRate:(a.coreTraits.learningRate+b.coreTraits.learningRate)/2,
+      darkAffinity:(a.coreTraits.darkAffinity+b.coreTraits.darkAffinity)/2
+    }
   };
-  return mutation.mutateGenome(baby);
 }
-
-module.exports = { mate };
+module.exports={ mate };
